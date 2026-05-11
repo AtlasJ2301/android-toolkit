4858c1edd28bd3d1f7b20c31fe91f21b59dc5baf
Atlas <atlasjunifer@protonmail.com>
2026-05-10 22:19:20 -0400

    curl

--- usr/src/curl.sh
+++ usr/src/curl.sh
@@ -2,10 +2,10 @@
 
 clear
 
-    rm -r ~/.android-toolkit/
+rm -r ~/.android-toolkit/
 
 git clone https://github.com/AtlasJ2301/android-toolkit ~/.android-toolkit/
 
-    sudo rm /usr/bin/android-toolkit
+sudo rm /usr/bin/android-toolkit
 
 sudo git clone https://github.com/AtlasJ2301/android-toolkit/blob/main/usr/bin/android-toolkit /usr/bin/android-toolkit
